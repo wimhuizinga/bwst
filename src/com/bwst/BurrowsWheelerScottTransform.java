ee074ed41b8221e798245c114601c29c0071aaf2
Wim Huizinga <wimhuizinga@gmail.com>
2018-08-24 11:28:27 +0200

    lcm comparator a bit quicker

--- src/com/bwst/BurrowsWheelerScottTransform.java
+++ src/com/bwst/BurrowsWheelerScottTransform.java
@@ -35,206 +35,206 @@ import java.util.List;
  */
 public class BurrowsWheelerScottTransform {
 
-	private static final LcmByteArrayComparator LCM_BYTE_ARRAY_COMPARATOR = new LcmByteArrayComparator();
-	
-	/*
-	 * Method to sort byte arrays (using least common multiple, not lexicographically)
-	 * Java only (uses Collections API with a custom comparator)
-	 */
-	private static void lcmSortByteArrays(byte[][] byteArrays) {
-		Collections.sort(Arrays.asList(byteArrays), LCM_BYTE_ARRAY_COMPARATOR);
-	}
-	
-	/*
-	 * Duval 1983, linear-time and O(1)-space. Converted from JavaScript
-	 * 
-	 * Ported from JavaScript:
-	 * http://www.allisons.org/ll/AlgDS/Strings/Factors/
-	 */
-	private static List<Integer> duval(byte[] bytes) {
-		int i, j, k = 0;
-		ArrayList<Integer> ans = new ArrayList<Integer>();
-		while (k < bytes.length) {
-			i = k;
-			j = k + 1;
-			while (true) {
-				if (j < bytes.length) {
-					byte str_i = bytes[i], str_j = bytes[j];
-					if (str_i < str_j) {
-						i = k;
-						j++;
-						continue;
-					} else if (str_i == str_j) {
-						i++;
-						j++;
-						continue;
-					}
-				}
-				do {
-					k += (j - i);
-					ans.add(k);
-				} while (k <= i);
-				break;
-			}
-		}
-		return ans;
-	}
-	
-	/*
-	 * Duval 1983, Generates factors of breaks
-	 * 
-	 * modified to work in Java
-	 */
-	private static byte[][] factor(List<Integer> breaks, byte[] bytes) {
-		byte[][] result = new byte[breaks.size()][];
-		Iterator<Integer> iter = breaks.iterator();
-		int i0 = 0, i1 = 0;
-		while (iter.hasNext()) {
-			int i2 = iter.next();
-			int length = i2 - i0;
-			byte[] arr = new byte[length];
-			System.arraycopy(bytes, i0, arr, 0, length);
-			result[i1] = arr;
-			i0 = i2;
-			i1++;
-		}
-		return result;
-	}
-		
-	/*
-	 * All rotations of single word
-	 */
-	private static byte[][] allRotations(byte[] bytes) {
-		byte[][] result = new byte[bytes.length][];
-		byte[] original = Arrays.copyOf(bytes, bytes.length);
-		result[0] = original;
-		byte[] next = Arrays.copyOf(original, original.length);
-		for(int i = 1; i < bytes.length; i++) {	
-			rotateRight(next);
-			result[i] = Arrays.copyOf(next, next.length);
-		}
-		return result;
-	}
-	
-	/*
-	 * All rotations of multiple words
-	 */
-	private static byte[][] allRotations(byte[][] bytesArr) {
-		int length = 0;
-		for(int i = 0; i < bytesArr.length; i++)
-			length += bytesArr[i].length;
-		byte[][] result = new byte[length][];
-		int index = 0;
-		for(int i = 0; i < bytesArr.length; i++) {
-			byte[][] rotations = allRotations(bytesArr[i]);
-			for(int j = 0; j < rotations.length; j++)
-				result[index++] = rotations[j]; 
-		}
-		return result;
-	}
-	
-	/*
-	 * Rotates single word to the right 
-	 * 
-	 * ABC -> CAB
-	 */
-	private static void rotateRight(byte[] bytes) {
-		byte temp = bytes[bytes.length - 1];
-		System.arraycopy(bytes, 0, bytes, 1, bytes.length - 1);
-		bytes[0] = temp;
-	}
-	
-	/*
-	 * Returns last bytes in words:
-	 * 
-	 * ABC
-	 * AC
-	 * AB
-	 * 
-	 * returns [ C, C, B ]
-	 */
-	private static byte[] lastOf(byte[][] bytesArr) {
-		byte[] result = new byte[bytesArr.length];
-		for(int i = 0; i < bytesArr.length; i++)
-			result[i] = bytesArr[i][bytesArr[i].length - 1];
-		return result;
-	}
-	
-	
-	/**
-	 * Encodes bytes to Burrows-Wheeler-Scott Transform
-	 * 
-	 * @param bytes Bytes to encode
-	 * @return Encoded bytes
-	 */
-	public static byte[] encode(byte[] bytes) {
-		// 
-		byte[][] factors = factor(duval(bytes), bytes);
-		byte[][] rotations = allRotations(factors);
-		
-		lcmSortByteArrays(rotations);
-		return lastOf(rotations);
-	}
-	
-	/**
-	 * Decodes bytes from Burrows-Wheeler-Scott Transform
-	 *
-	 * @param bytes Bytes to encode
-	 * @return Encoded bytes
-	 */
-	public static byte[] decode(byte[] bytes) {
-		byte[] sort = Arrays.copyOf(bytes, bytes.length);
-		Arrays.sort(sort);
-		int[] map = new int[bytes.length];
-		for(int i0 = 0; i0 < bytes.length; i0++) {
-			byte b = bytes[i0];
-			int s = 0;
-			for(int i1 = 0; i1 < sort.length; i1++) {
-				if(b == sort[i1]) {
-					s = i1;
-					break;
-				}
-			}
-			int c = 0;
-			for(int j = 0; j < i0; j++)
-				if(bytes[j] == b)
-					c++;
-			int r = s + c;
-			map[r] = i0;
-		}
-		int s = 0, index = 0;
-		boolean[] visited = new boolean[map.length];
-		byte[] result = new byte[bytes.length];
-		int length = 0, next = bytes.length;
-		for(int i = map.length - 1; i > -1; i--) {
-			visited[index] = true;
-			length++;			
-			result[i] = sort[index];
-			if(map[index] == s) {
-				// word found
-				next -= length;
-				byte[] reversed = new byte[length];
-				for(int k = 0; k < length; k++)
-					reversed[length - 1 - k] = result[next + k];
-				System.arraycopy(reversed, 0, result, next, length);
-				int j = 0;
-				while(j < map.length && visited[j]) // find next start
-					j++;
-				s = j;
-				index = j;
-				length = 0;
-			} else
-				index = map[index];
-		}
-		return result;
-	}
-	
-	private static String str(byte[] bytes) {
-		return new String(bytes);
-	}
-	
-	public static void main(String[] args) {
-	    String original = "BANANA";
+    private static final LcmByteArrayComparator LCM_BYTE_ARRAY_COMPARATOR = new LcmByteArrayComparator();
+
+    /*
+     * Method to sort byte arrays (using least common multiple, not lexicographically)
+     * Java only (uses Collections API with a custom comparator)
+     */
+    private static void lcmSortByteArrays(byte[][] byteArrays) {
+        Collections.sort(Arrays.asList(byteArrays), LCM_BYTE_ARRAY_COMPARATOR);
+    }
+
+    /*
+     * Duval 1983, linear-time and O(1)-space. Converted from JavaScript
+     *
+     * Ported from JavaScript:
+     * http://www.allisons.org/ll/AlgDS/Strings/Factors/
+     */
+    private static List<Integer> duval(byte[] bytes) {
+        int i, j, k = 0;
+        ArrayList<Integer> ans = new ArrayList<Integer>();
+        while (k < bytes.length) {
+            i = k;
+            j = k + 1;
+            while (true) {
+                if (j < bytes.length) {
+                    byte str_i = bytes[i], str_j = bytes[j];
+                    if (str_i < str_j) {
+                        i = k;
+                        j++;
+                        continue;
+                    } else if (str_i == str_j) {
+                        i++;
+                        j++;
+                        continue;
+                    }
+                }
+                do {
+                    k += (j - i);
+                    ans.add(k);
+                } while (k <= i);
+                break;
+            }
+        }
+        return ans;
+    }
+
+    /*
+     * Duval 1983, Generates factors of breaks
+     *
+     * modified to work in Java
+     */
+    private static byte[][] factor(List<Integer> breaks, byte[] bytes) {
+        byte[][] result = new byte[breaks.size()][];
+        Iterator<Integer> iter = breaks.iterator();
+        int i0 = 0, i1 = 0;
+        while (iter.hasNext()) {
+            int i2 = iter.next();
+            int length = i2 - i0;
+            byte[] arr = new byte[length];
+            System.arraycopy(bytes, i0, arr, 0, length);
+            result[i1] = arr;
+            i0 = i2;
+            i1++;
+        }
+        return result;
+    }
+
+    /*
+     * All rotations of single word
+     */
+    private static byte[][] allRotations(byte[] bytes) {
+        byte[][] result = new byte[bytes.length][];
+        byte[] original = Arrays.copyOf(bytes, bytes.length);
+        result[0] = original;
+        byte[] next = Arrays.copyOf(original, original.length);
+        for(int i = 1; i < bytes.length; i++) {
+            rotateRight(next);
+            result[i] = Arrays.copyOf(next, next.length);
+        }
+        return result;
+    }
+
+    /*
+     * All rotations of multiple words
+     */
+    private static byte[][] allRotations(byte[][] bytesArr) {
+        int length = 0;
+        for(int i = 0; i < bytesArr.length; i++)
+            length += bytesArr[i].length;
+        byte[][] result = new byte[length][];
+        int index = 0;
+        for(int i = 0; i < bytesArr.length; i++) {
+            byte[][] rotations = allRotations(bytesArr[i]);
+            for(int j = 0; j < rotations.length; j++)
+                result[index++] = rotations[j];
+        }
+        return result;
+    }
+
+    /*
+     * Rotates single word to the right
+     *
+     * ABC -> CAB
+     */
+    private static void rotateRight(byte[] bytes) {
+        byte temp = bytes[bytes.length - 1];
+        System.arraycopy(bytes, 0, bytes, 1, bytes.length - 1);
+        bytes[0] = temp;
+    }
+
+    /*
+     * Returns last bytes in words:
+     *
+     * ABC
+     * AC
+     * AB
+     *
+     * returns [ C, C, B ]
+     */
+    private static byte[] lastOf(byte[][] bytesArr) {
+        byte[] result = new byte[bytesArr.length];
+        for(int i = 0; i < bytesArr.length; i++)
+            result[i] = bytesArr[i][bytesArr[i].length - 1];
+        return result;
+    }
+
+
+    /**
+     * Encodes bytes to Burrows-Wheeler-Scott Transform
+     *
+     * @param bytes Bytes to encode
+     * @return Encoded bytes
+     */
+    public static byte[] encode(byte[] bytes) {
+        //
+        byte[][] factors = factor(duval(bytes), bytes);
+        byte[][] rotations = allRotations(factors);
+
+        lcmSortByteArrays(rotations);
+        return lastOf(rotations);
+    }
+
+    /**
+     * Decodes bytes from Burrows-Wheeler-Scott Transform
+     *
+     * @param bytes Bytes to encode
+     * @return Encoded bytes
+     */
+    public static byte[] decode(byte[] bytes) {
+        byte[] sort = Arrays.copyOf(bytes, bytes.length);
+        Arrays.sort(sort);
+        int[] map = new int[bytes.length];
+        for(int i0 = 0; i0 < bytes.length; i0++) {
+            byte b = bytes[i0];
+            int s = 0;
+            for(int i1 = 0; i1 < sort.length; i1++) {
+                if(b == sort[i1]) {
+                    s = i1;
+                    break;
+                }
+            }
+            int c = 0;
+            for(int j = 0; j < i0; j++)
+                if(bytes[j] == b)
+                    c++;
+            int r = s + c;
+            map[r] = i0;
+        }
+        int s = 0, index = 0;
+        boolean[] visited = new boolean[map.length];
+        byte[] result = new byte[bytes.length];
+        int length = 0, next = bytes.length;
+        for(int i = map.length - 1; i > -1; i--) {
+            visited[index] = true;
+            length++;
+            result[i] = sort[index];
+            if(map[index] == s) {
+                // word found
+                next -= length;
+                byte[] reversed = new byte[length];
+                for(int k = 0; k < length; k++)
+                    reversed[length - 1 - k] = result[next + k];
+                System.arraycopy(reversed, 0, result, next, length);
+                int j = 0;
+                while(j < map.length && visited[j]) // find next start
+                    j++;
+                s = j;
+                index = j;
+                length = 0;
+            } else
+                index = map[index];
+        }
+        return result;
+    }
+
+    private static String str(byte[] bytes) {
+        return new String(bytes);
+    }
+
+    public static void main(String[] args) {
+//      String original = "BANANA";
 
 //		String original = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vitae ex auctor lectus gravida pretium. Vestibulum at condimentum neque. Mauris libero augue, mattis et pretium eget, fermentum vel ante. Phasellus consectetur diam dignissim dignissim ornare. Suspendisse potenti. Ut arcu lacus, aliquam id dolor a, viverra facilisis augue. Ut vitae tincidunt justo. Nullam dignissim metus in eros cursus imperdiet. Praesent aliquam dapibus elit et iaculis. Fusce enim magna, venenatis ut molestie et, gravida at nunc. Integer consectetur nisi et luctus porttitor. In eros odio, euismod sed neque eu, euismod bibendum ligula. In in dui in nisl ultrices vehicula nec id enim. Praesent sed dolor sit amet odio dictum bibendum." +
 //						  "Sed dapibus dui eu velit scelerisque scelerisque. Pellentesque et elit risus. Morbi luctus metus sollicitudin, pretium libero vel, faucibus mauris. Proin arcu nisl, feugiat et gravida vitae, convallis at dolor. Fusce pharetra tempus pellentesque. Nam eget ultricies mi. Nullam tempor ultrices nibh, nec viverra sapien imperdiet ac. Nulla molestie pulvinar est, at tempor quam sagittis non. Mauris sollicitudin ullamcorper metus. Sed ac consectetur mi. Pellentesque imperdiet imperdiet dolor et pellentesque." +
@@ -242,14 +242,14 @@ public class BurrowsWheelerScottTransform {
 //						  "Nam eu egestas leo, condimentum tristique tortor. Donec eget ex ac lacus condimentum feugiat et et purus. Aliquam a mauris molestie, fringilla massa ac, facilisis quam. Cras hendrerit risus nulla, vel ornare tellus condimentum at. Sed turpis sapien, rutrum sed ligula sit amet, venenatis interdum justo. Fusce sodales sit amet mauris sollicitudin malesuada. Aliquam luctus mauris vel pulvinar elementum. Pellentesque vitae laoreet metus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed lacinia lacus sit amet risus feugiat varius. Pellentesque fringilla elit nec accumsan faucibus. Suspendisse sodales elit nec mi pellentesque, in sodales eros venenatis." +
 //						  "Duis gravida ante non tincidunt placerat. Morbi congue tempor tortor, vel congue lectus volutpat hendrerit. Suspendisse sit amet arcu hendrerit, mattis purus et, tincidunt leo. Aliquam finibus sem sed tortor vulputate faucibus. Etiam id dui ac ante finibus malesuada. Aenean tempor risus augue, et ullamcorper turpis sollicitudin eu. Mauris dignissim posuere ipsum vel bibendum. Fusce ante ligula, iaculis at quam in, congue ultrices nisl. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam sit amet leo vel mi rutrum posuere. Donec ornare, eros non condimentum lobortis, tellus ex feugiat diam, eu lobortis felis erat et lectus.";
 
-//		String original = "ABCBDBDBBABDBBABCDBCDDABCDBBBCDBBDBDBBABBCBD";
+		String original = "BACK1BACKBAC3KBACKBA4CKBA5BACK6BAC6K";
 
         byte[] encoded = encode(original.getBytes());
 
-		System.out.println("original: " + original);
-		System.out.println("encoded:  " + str(encoded));
-		System.out.println();
-		System.out.println("decoded:  " + str(decode(encoded)));
-		System.out.println("original: " + original);
-	}
+        System.out.println("original: " + original);
+        System.out.println("encoded : " + str(encoded));
+        System.out.println();
+        System.out.println("decoded : " + str(decode(encoded)));
+        System.out.println("original: " + original);
+    }
 }
--- src/com/bwst/LcmByteArrayComparator.java
+++ src/com/bwst/LcmByteArrayComparator.java
@@ -3,41 +3,48 @@ package com.bwst;
 import java.util.Comparator;
 
 /**
- * v0.1 initial version 19-08-2018
+ * v0.1.2 removed the if, added if statements to 0-reset to reduce if-checking 24-08-2018
+ * v0.1.1 removed gcd and lcm code, added if that waits for iteration end 23-08-2018
+ * v0.1.0 initial version 19-08-2018
  *
  * Compares byte arrays of virtually infinite length.
- * Stops comparing when least common multiple is found.
+ * Stops comparing when least common multiple is found
+ * (iteration of both arrays end at the same time)
+ * to simulate the infinite length.
  *
  * @author Wim Huizinga 
  * 
  */
 public class LcmByteArrayComparator implements Comparator<byte[]> {
 
-	private int x, y, c;
-	private byte a, b;
+    private int x, y, c;
+    private byte a, b;
 
-	@Override
-	public int compare(byte[] o1, byte[] o2) {
-		x = 0;
-		y = 0;
+    @Override
+    public int compare(byte[] o1, byte[] o2) {
+        x = 0;
+        y = 0;
         while(true) { // lcm will break this loop
-			a = o1[x];
-			b = o2[y];
+            a = o1[x];
+            b = o2[y];
             c = Byte.compare(a, b);
-			if(c != 0) {
+            if(c != 0) {
                 return c;
             }
-			x++;
+            x++;
             y++;
 
-            if(x == o1.length && y == o2.length) // lcm found
-                break;
-
-			if(x == o1.length)
-				x = 0;
-			if(y == o2.length)
-				y = 0;
-		}
-		return 0; // they were equal
-	}
+            if(x == o1.length) {
+                if(y == o2.length) // lcm found x == o1.length and y == o2.length
+                    break;
+                x = 0;
+            }
+            if(y == o2.length) {
+                if(x == o1.length) // lcm found x == o1.length and y == o2.length
+                    break;
+                y = 0;
+            }
+        }
+        return 0; // a and b were equal
+    }
 }
